734438485650b9ee0fbb51ea55d97c0aa2d7f6f0
Arun Durvasula <adurvasu@farm.cse.ucdavis.edu>
2014-07-07 22:46:59 -0700

    changes for Hapmap3

--- scripts/angsdo.sh
+++ scripts/angsdo.sh
@@ -25,7 +25,7 @@ glikehood=1
 minMapQ=30
 cpu=32
 #range=""
-range="-r 1:100000"
+range="-r 1:"
 
 #(estimate an SFS)
 # -bam list of paths to bamfiles you want to use
@@ -42,9 +42,9 @@ range="-r 1:100000"
 # -P 8 use 8 threads
 # -indF individiual inbreeding coefficient. for inbred lines just make a files of "1" on each line for each bamfile. otherwise use ngsF to estimate (see inbreeding.sh script)
 
-command1="-bam data/"$taxon"_list.txt -out temp/"$taxon" -doMajorMinor 1 -doMaf 1 -indF data/$taxon.indF -doSaf 1 -uniqueOnly 0 -anc data/TRIP.fa.gz -minMapQ $minMapQ -minQ 20 -nInd $nInd -minInd $minInd -baq 1 -ref /home/adurvasu/genomes/Zea_mays.AGPv3.21.dna.genome.fa.gz -GL $glikehood -P $cpu  $range"
+command1="-bam data/"$taxon"_list.txt -out temp/"$taxon" -doMajorMinor 1 -doMaf 1 -indF data/$taxon.indF -doSaf 1 -uniqueOnly 0 -anc data/TRIP.fa.gz -minMapQ $minMapQ -minQ 20 -nInd $nInd -minInd -baq 1 -ref /home/adurvasu/genomes/Zea_mays.AGPv3.21.dna.genome.fa.gz -GL $glikehood -P $cpu  $range"
 echo $command1
-$angsdir/angsd $command1
+time $angsdir/angsd $command1
 
 # not clear to me how to run folded, as -fold option seems to be deprecated?
 # temp/"$taxon"_pest.saf output file from above run; prior on SFS?
@@ -57,7 +57,7 @@ $angsdir/angsd $command1
 	# remaining 5% are polymorphic
 command2="temp/"$taxon".saf $n -P $cpu" 
 echo $command2
-$angsdir/misc/emOptim2 $command2 > results/"$taxon".sfs
+time $angsdir/misc/emOptim2 $command2 > results/"$taxon".sfs
 
 #(calculate thetas)
 # this now uses the SFS to calculate stats
@@ -71,15 +71,15 @@ $angsdir/misc/emOptim2 $command2 > results/"$taxon".sfs
 #10      3371    -8.822116       -10.395367      -7.431857       -14.041094      -11.062746
 #10      3372    -8.840759       -10.415518      -7.448764       -14.064022      -11.082968
 #10	26926	-1.480456	-0.671793	-211.328599	-0.694813	-0.683237
-#command3="-bam data/"$taxon"_list.txt -out results/"$taxon" -doThetas 1 -doSaf 1 -GL $glikehood -indF data/$taxon.indF -pest results/"$taxon".sfs -anc data/TRIP.fa.gz -uniqueOnly 0 -minMapQ $minMapQ -minQ 20 -nInd $nInd -minInd $minInd -baq 1 -ref /home/jri/genomes/Zea_mays.AGPv2.17.dna.toplevel.fa -P $cpu $range"
-#echo $command3
-#$angsdir/angsd $command3 
+command3="-bam data/"$taxon"_list.txt -out results/"$taxon" -doThetas 1 -doSaf 1 -GL $glikehood -indF data/$taxon.indF -pest results/"$taxon".sfs -anc data/TRIP.fa.gz -uniqueOnly 0 -minMapQ $minMapQ -minQ 20 -nInd $nInd -minInd $minInd -baq 1 -ref  /home/adurvasu/genomes/Zea_mays.AGPv3.21.dna.genome.fa.gz -P $cpu $range"
+echo $command3
+time $angsdir/angsd $command3 
 
 #(calculate Tajimas.)
 # this estiamtes TajD and other stats and makes a sortof bedfile output
-#command4=" make_bed results/"$taxon".thetas.gz results/"$taxon""
-#echo $command4
-#$angsdir/misc/thetaStat $command4 
+command4=" make_bed results/"$taxon".thetas.gz results/"$taxon""
+echo $command4
+time $angsdir/misc/thetaStat $command4 
 
 # this does a sliding window analysis
 # -nChr number of chromosomes
@@ -89,6 +89,6 @@ $angsdir/misc/emOptim2 $command2 > results/"$taxon".sfs
 #(569,1175)(4000,5001)(4000,5000)        10      4500    4.536109        4.774793        1.392152        9.523595        7.149193        0.169980        1.0433
 #(963,1565)(4706,5500)(4500,5500)        10      5000    2.850285        2.665415        1.624860        4.608032        3.636723        -0.196105       0.4532
 # with information for each window (see ANGSD online documentation for some explanation of columns)
-#command5="do_stat results/"$taxon" -nChr $n -win $windowsize -step $step"
-#echo $command5
-#$angsdir/misc/thetaStat 
+command5="do_stat results/"$taxon" -nChr $n -win $windowsize -step $step"
+echo $command5
+time $angsdir/misc/thetaStat 
